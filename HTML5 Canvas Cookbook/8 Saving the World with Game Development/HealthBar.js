32a1395f1336d937fe5a4001a50084fb8bc5cb55
mcli28 <mclimber28@gmail.com>
2019-06-13 20:07:06 -0400

    13/06/2019

---
+++ HTML5 Canvas Cookbook/8 Saving the World with Game Development/HealthBar.js
@@ -0,0 +1,31 @@
+/*
+ * HealthBar class should have no knowledge
+ * of the Actor or Level classes to
+ * keep it decoupled
+ */
+function HealthBar(config) {
+    this.controller = config.controller;
+    this.maxHealth = config.maxHealth;
+    this.x = config.x;
+    this.y = config.y;
+    this.maxWidth = config.maxWidth;
+    this.height = config.height;
+    this.health = this.maxHealth;
+}
+HealthBar.prototype.setHealth = function (health) {
+    this.health = health;
+};
+HealthBar.prototype.draw = function () {
+    var context = this.controller.view.context;
+    context.beginPath();
+    context.rect(this.x, this.y, this.maxWidth, this.height);
+    context.fillStyle = "black";
+    context.fill();
+    context.closePath();
+    context.beginPath();
+    var width = this.maxWidth * this.health / this.maxHealth;
+    context.rect(this.x, this.y, width, this.height);
+    context.fillStyle = "red";
+    context.fill();
+    context.closePath();
+};
